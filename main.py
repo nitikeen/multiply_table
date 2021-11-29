e6c52ab8581da13b9ee7574797cd3101c7e01f45
voidZoich <nikitinkv@mail.ru>
2021-11-29 22:44:48 +0500

    Добавил запись и загрузку статистики в файл

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ import json
 
 op = ('mul', 'dev')
 
-stat = dict(mul={a + 1: {b + 1: 100 for b in range(9)} for a in range(9)},
-            dev={a + 1: {b + 1: 100 for b in range(9)} for a in range(81)})
+try:
+    with open("stat.json", "r") as read_file:
+        stat = json.load(read_file)
+except IOError:
+    stat = dict(mul={a + 1: {b + 1: 100 for b in range(9)} for a in range(9)},
+                dev={a + 1: {b + 1: 100 for b in range(9)} for a in range(81)})
+
 print(stat)
 print('Тренажер таблицы умножения и деления')
 while True:  # Повторяем опрос
@@ -21,7 +26,7 @@ while True:  # Повторяем опрос
             a = b * random.randint(1, 9)
             fx = f'{a} : {b}'
             result = a / b
-        if random.randint(0, 100) <= stat[operation][a][b]:
+        if random.randint(0, 100) <= stat[operation][str(a)][str(b)]:
             break
     while True:
         try:
@@ -37,7 +42,9 @@ while True:  # Повторяем опрос
             else:
                 print(f'Не верно, {round(result)}')
                 answer_duration = 100
-            stat[operation][a][b] = (stat[operation][a][b] + answer_duration) / 2
-            print(stat)
+                time.sleep(2)
+            stat[operation][str(a)][str(b)] = (stat[operation][str(a)][str(b)] + answer_duration) / 2
+            with open("stat.json", "w") as write_file:
+                json.dump(stat, write_file, indent=4)
             time.sleep(2)
             break
