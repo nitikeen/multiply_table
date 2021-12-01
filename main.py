7c126f6f25d716e581f818aae60f6c2d8348f333
voidZoich <nikitinkv@mail.ru>
2021-12-01 19:34:03 +0500

    Ввел уровни

--- main.py
+++ main.py
@@ -3,24 +3,47 @@ from datetime import datetime
 import time
 import json
 
+def level(stat):
+    result = 9
+    for a in stat['mul']:
+        total = 0
+        for b in stat['mul'][a]:
+            total += stat['mul'][a][b]
+            if stat['mul'][a][b] > 30 and result > int(a):
+                result = int(a)
+        dev = total / len(stat['mul'][a])
+    for a in stat['dev']:
+        total_dev = dict()
+        for b in stat['dev'][a]:
+            # total_dev[b] += stat['dev'][a][b]
+            if stat['dev'][a][b] > 30 and result > int(b):
+                result = int(b)
+    return result
+
+
 op = ('mul', 'dev')
 
+print('Тренажер таблицы умножения и деления')
 try:  # Открываем файл со статистикой
     with open("stat.json", "r") as read_file:
         stat = json.load(read_file)
 except IOError:
     stat = dict()
-
-print('Тренажер таблицы умножения и деления')
+current_level = level(stat)
+start_level = 0
 while True:  # Повторяем опрос
+    if start_level != current_level:
+        print(f'Вы на уровне {current_level}')
+        start_level = current_level
     while True:  # Генерируем задачу
-        b = random.randint(1, 9)
         operation = random.choice(op)
         if operation == 'mul':  # Умножение
-            a = random.randint(1, 9)
+            a = random.randint(1, current_level)
+            b = random.randint(1, 9)
             fx = f'{a} x {b}'
             result = a * b
         else:  # Деление
+            b = random.randint(1, current_level)
             a = b * random.randint(1, 9)
             fx = f'{a} : {b}'
             result = a / b
@@ -55,4 +78,5 @@ while True:  # Повторяем опрос
             with open("stat.json", "w") as write_file:
                 json.dump(stat, write_file, indent=4, sort_keys=True)
             time.sleep(2)
+            current_level = level(stat)
             break
